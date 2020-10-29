8c5cd0188b80a247f0bd36c340bc383e60c9f8d3
rasmuswind-ai <73473969+rasmuswind-ai@users.noreply.github.com>
2020-10-29 09:17:01 +0100

    Update CombinedFunctions.psm1

--- CombinedFunctions/CombinedFunctions.psm1
+++ CombinedFunctions/CombinedFunctions.psm1
@@ -35,138 +35,152 @@
         This CMDLet runs different lines of code to list the currently logged on users, and their session time start date.
     #>
     function Get-LoggedOnUsers {
+
+
         #Gets the CimInstance that shows logged on users
         #? {$_.LastLogon -ne $null} = Excludes the users that has a null value in "LastLogon" (This is usually the user: NT AUTHORITY\System)
-        #Selects the values: "Caption, FullName and UserType    
-         #$CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "UserType"
+        #Selects the values: Caption, FullName, UserType and the date when the password expires
+        $CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "UserType","PasswordExpires"
+
+  
+
+        #For each entry (Item) in $CimInstance_LoggedOnUsers - Create new object and insert the following values for each item, in that object
+        $LoggedOnUsers = @()
+
+        foreach ($item in $CimInstance_LoggedOnUsers)
+                         {
         
-        #Collects the Users session and filters the Negotiate authenticationpackage away from the object and selects "StartTime"
-        #$SessionStartTime = Get-CimInstance -ClassName Win32_session | ? {$_.AuthenticationPackage -ne "Negotiate"} | Select-Object -Property "StartTime"
-        
-        #For each Entry in the list, create a new object and make the new object with the added Session StartTime.
-        Get-CimInstance -ClassName Win32_NetworkLoginProfile | ForEach-Object{
-            $CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "UserType" 
-            $SessionStartTime = Get-CimInstance -ClassName Win32_session | ? {$_.AuthenticationPackage -ne "Negotiate"} | Select-Object -Property "StartTime"
-    
-            $NewObject = New-Object -TypeName psobject -Property @{
-                Caption = $CimInstance_LoggedOnUsers.Caption
-                FullName = $CimInstance_LoggedOnUsers.FullName
-                UserType = $CimInstance_LoggedOnUsers.UserType
-                SessionStartDate = $SessionStartTime.StartTime
-                
-            }   
-        }
-        #Select-Object to adjust the columns to be in the right order.
-        $NewObject | Select-Object FullName,Caption,UserType,SessionStartDate
-        
+                                        
+                             $CustomObject = New-Object PSCustomObject
+                             $CustomObject | Add-Member -Type NoteProperty -Name Caption -Value $Item.Caption
+                             $CustomObject | Add-Member -Type NoteProperty -Name FullName -Value $Item.FullName
+                             $CustomObject | Add-Member -Type NoteProperty -Name UserType -Value $Item.UserType
+                             $CustomObject | Add-Member -Type NoteProperty -Name PasswordExpires -Value $item.PasswordExpires
+                            
+                             #Adds the $CustomObject values to the $LoggedOnUsers variable
+                             $LoggedOnUsers += $CustomObject
+                             Clear-Variable -Name CustomObject
+                        } 
+    $LoggedOnUsers
 }
 
 
 
-<#
-.SYNOPSIS
-    Lists the currently logged on users and the Number of Logons for each user.
-.DESCRIPTION
-    This CMDLet lists all currently logged on users, their fullname and their Username.
-    It will also list the number of logons.
-.EXAMPLE
-     Get-NumberOfLogons
+    <#
+    .SYNOPSIS
+        Lists the currently logged on users and the Number of Logons for each user.
+    .DESCRIPTION
+        This CMDLet lists all currently logged on users, their fullname and their Username.
+        It will also list the number of logons.
+    .EXAMPLE
+         Get-NumberOfLogons
 
-    See if specific user is logged in (Use wildcard (*) If the whole name is not known.)
-    Get-LoggedonUsers | ?{$_.FullName -like "Rasmus*"}
-.EXAMPLE
-    N/A
-.INPUTS
-    N/A
-.OUTPUTS
-    Output of the CMDLet: Get-NumberOfLogons | ?{$_.FullName -like "Rasmus*"} (This will include all the users that has a fullname that starts with "Rasmus")
+        See if specific user is logged in (Use wildcard (*) If the whole name is not known.)
+        Get-LoggedonUsers | ?{$_.FullName -like "Rasmus*"}
+    .EXAMPLE
+        N/A
+    .INPUTS
+        N/A
+    .OUTPUTS
+        Output of the CMDLet: Get-NumberOfLogons | ?{$_.FullName -like "Rasmus*"} (This will include all the users that has a fullname that starts with "Rasmus")
 
-    FullName    Caption  NumberOfLogons
-    --------    -------  --------------
-    Rasmus Wind rasm757j            270
-.NOTES
-    This is a 'Get' CMDLet, therefore no system changes are being made while running
-.COMPONENT
-    N/A
-.ROLE
-    N/A
-.FUNCTIONALITY
-    This CMDLet runs different lines of code to list the currently logged on users, and their number of logons.
-#>
-function Get-NumberOfLogons {
-    #Gets the CimInstance that shows logged on users
-    #? {$_.LastLogon -ne $null} = Excludes the users that has a null value in "LastLogon" (This is usually the user: NT AUTHORITY\System)
-    #Selects the values: "Caption, FullName and NumberOfLogons    
-     #$CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "NumberOfLogons"
+        FullName    Caption  NumberOfLogons
+        --------    -------  --------------
+        Rasmus Wind rasm757j            270
+    .NOTES
+        This is a 'Get' CMDLet, therefore no system changes are being made while running
+    .COMPONENT
+        N/A
+    .ROLE
+        N/A
+    .FUNCTIONALITY
+        This CMDLet runs different lines of code to list the currently logged on users, and their number of logons.
+    #>
+    function Get-NumberOfLogons {
 
+            #Gets the CimInstance that shows logged on users
+            #? {$_.LastLogon -ne $null} = Excludes the users that has a null value in "LastLogon" (This is usually the user: NT AUTHORITY\System)
+            #Selects the values: "Caption, FullName and NumberOfLogons    
+            #$CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "NumberOfLogons"
+            $CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "NumberOfLogons" 
     
-    #For each Entry in the list, create a new object and make the new object with the added Number of Logons.
-    Get-CimInstance -ClassName Win32_NetworkLoginProfile | ForEach-Object{
-        $CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "NumberOfLogons" 
+            #For each Entry in the list, create a new object and make the new object with the added Number of Logons.
+            $NumberOfLogons = @()
 
-        $NewObject = New-Object -TypeName psobject -Property @{
-            Caption = $CimInstance_LoggedOnUsers.Caption
-            FullName = $CimInstance_LoggedOnUsers.FullName
-            NumberOfLogons = $CimInstance_LoggedOnUsers.NumberOfLogons
+            foreach ($item in $CimInstance_LoggedOnUsers)
+                             {
             
-        }
-    }
-    #Select-Object to adjust the columns to be in the right order.
-    $NewObject | Select-Object FullName,Caption,NumberOfLogons
-    
+                                            
+                                 $CustomObject = New-Object PSCustomObject
+                                 $CustomObject | Add-Member -Type NoteProperty -Name Caption -Value $Item.Caption
+                                 $CustomObject | Add-Member -Type NoteProperty -Name FullName -Value $Item.FullName
+                                 $CustomObject | Add-Member -Type NoteProperty -Name UserType -Value $Item.UserType
+                                 $CustomObject | Add-Member -Type NoteProperty -Name NumberOfLogons -Value $Item.NumberOfLogons
+                                
+                                 #Adds the $CustomObject values to the $NumberOfLogons variable
+                                 $NumberOfLogons += $CustomObject
+                                 Clear-Variable -Name CustomObject
+                            } 
+       $NumberOfLogons
 }
 
 
-<#
-.SYNOPSIS
-    Lists the currently logged on users and the date that they last logged on.
-.DESCRIPTION
-    This CMDLet lists all currently logged on users, their fullname and their Username.
-    It will also list the last logon date.
-.EXAMPLE
-     Get-LastLogon
+    <#
+    .SYNOPSIS
+        Lists the currently logged on users and the date that they last logged on.
+    .DESCRIPTION
+        This CMDLet lists all currently logged on users, their fullname and their Username.
+        It will also list the last logon date.
+    .EXAMPLE
+         Get-LastLogon
 
-    See if specific user is logged in (Use wildcard (*) If the whole name is not known.)
-    Get-LastLogon | ?{$_.FullName -like "Rasmus*"}
-.EXAMPLE
-    N/A
-.INPUTS
-    N/A
-.OUTPUTS
-    Output of the CMDLet: Get-LastLogon | ?{$_.FullName -like "Rasmus*"} (This will include all the users that has a fullname that starts with "Rasmus")
+        See if specific user is logged in (Use wildcard (*) If the whole name is not known.)
+        Get-LastLogon | ?{$_.FullName -like "Rasmus*"}
+    .EXAMPLE
+        N/A
+    .INPUTS
+        N/A
+    .OUTPUTS
+        Output of the CMDLet: Get-LastLogon | ?{$_.FullName -like "Rasmus*"} (This will include all the users that has a fullname that starts with "Rasmus")
 
-    FullName    Caption  LastLogon
-    --------    -------  ---------
-    Rasmus Wind rasm757j 27-10-2020 12:28:20
-.NOTES
-    This is a get CMDLet, therefore no system changes are being made while running
-.COMPONENT
-    N/A
-.ROLE
-    N/A
-.FUNCTIONALITY
-    This CMDLet runs different lines of code to list the currently logged on users, and their last logon date.
-#>
-function Get-LastLogon {
-    #Gets the CimInstance that shows logged on users
-    #? {$_.LastLogon -ne $null} = Excludes the users that has a null value in "LastLogon" (This is usually the user: NT AUTHORITY\System)
-    #Selects the values: "Caption, FullName and Last Logon    
-     #$CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "LastLogon"
+        FullName    Caption  LastLogon
+        --------    -------  ---------
+        Rasmus Wind rasm757j 27-10-2020 12:28:20
+    .NOTES
+        This is a get CMDLet, therefore no system changes are being made while running
+    .COMPONENT
+        N/A
+    .ROLE
+        N/A
+    .FUNCTIONALITY
+        This CMDLet runs different lines of code to list the currently logged on users, and their last logon date.
+    #>
+    function Get-LastLogon {
+
+            #Gets the CimInstance that shows logged on users
+            #? {$_.LastLogon -ne $null} = Excludes the users that has a null value in "LastLogon" (This is usually the user: NT AUTHORITY\System)
+            #Selects the values: "Caption, FullName and Last Logon    
+            #$CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "LastLogon"
+            $CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "LastLogon" 
     
-    #For each Entry in the list, create a new object and make the new object with the added Last Logon date.
-    Get-CimInstance -ClassName Win32_NetworkLoginProfile | ForEach-Object{
-        $CimInstance_LoggedOnUsers = Get-CimInstance -ClassName Win32_networkloginprofile | ? {$_.LastLogon -ne $null} | Select-Object -Property "Caption", "FullName", "LastLogon" 
+            #For each Entry in the list, create a new object and make the new object with the added Last Logon date.
+            $LastLogon = @()
 
-        $NewObject = New-Object -TypeName psobject -Property @{
-            Caption = $CimInstance_LoggedOnUsers.Caption
-            FullName = $CimInstance_LoggedOnUsers.FullName
-            LastLogon = $CimInstance_LoggedOnUsers.LastLogon
+            foreach ($item in $CimInstance_LoggedOnUsers)
+                             {
             
-        }   
-    }
-    #Select-Object to adjust the columns to be in the right order.
-    $NewObject | Select-Object FullName,Caption,LastLogon
-    
+                                            
+                                 $CustomObject = New-Object PSCustomObject
+                                 $CustomObject | Add-Member -Type NoteProperty -Name Caption -Value $Item.Caption
+                                 $CustomObject | Add-Member -Type NoteProperty -Name FullName -Value $Item.FullName
+                                 $CustomObject | Add-Member -Type NoteProperty -Name UserType -Value $Item.UserType
+                                 $CustomObject | Add-Member -Type NoteProperty -Name LastLogon -Value $Item.LastLogon
+                                
+                                 #Adds the $CustomObject values to the $LastLogon variable
+                                 $LastLogon += $CustomObject
+                                 Clear-Variable -Name CustomObject
+                            } 
+       $LastLogon
 }
 
 Export-ModuleMember -Function "*"
